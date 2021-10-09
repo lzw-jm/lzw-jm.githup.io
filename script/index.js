5365f32f431920239ccb695587725c851c96b14d
qq1019708531 <92204622+qq1019708531@users.noreply.github.com>
2021-10-10 07:57:34 +0800

    Create index.js

---
+++ script/index.js
@@ -0,0 +1,5 @@
+    var outer = document.getElementById('outer');
+    var footer = document.querySelector('.footer');
+    footer.onclick = function(){
+        outer.setAttribute('class','top-up');
+    }
