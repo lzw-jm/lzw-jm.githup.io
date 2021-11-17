9b57b275c418c68559ea53c6da595177fc217a67
qq1019708531 <1019708531@qq.com>
2021-11-17 09:54:59 +0800

    修改

--- src/content.js
+++ src/content.js
@@ -3,31 +3,32 @@ import sectext from './html/sectext.html'
 import thrtext from './html/thrtext.html'
 import fourtext from './html/fourtext.html'
 import fivetext from './html/fivetext.html'
-function parseHTML(text){
-     let oDiv = document.createElement('div');
-     oDiv.innerHTML = text;
-     let newText = '';
+
+function parseHTML(text) {
+    let oDiv = document.createElement('div');
+    oDiv.innerHTML = text;
+    let newText = '';
     newText = oDiv.childNodes[0].innerHTML;
-     return newText
+    return newText
 }
 export default [{
-    id: 1,
-    article:parseHTML(fristtext)
-},
-{
-    id: 2,
-    article: parseHTML(sectext)
-},
-{
-    id: 3,
-    article: parseHTML(thrtext)
-},
-{
-    id: 4,
-    article: parseHTML(fourtext)
-},
-{
-    id: 5,
-    article: parseHTML(fivetext)
-},
+        id: 1,
+        article: parseHTML(fristtext)
+    },
+    {
+        id: 2,
+        article: parseHTML(sectext)
+    },
+    {
+        id: 3,
+        article: parseHTML(thrtext)
+    },
+    {
+        id: 4,
+        article: parseHTML(fourtext)
+    },
+    {
+        id: 5,
+        article: parseHTML(fivetext)
+    },
 ]
